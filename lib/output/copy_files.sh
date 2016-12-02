9bcc6ebf87d0740ed1a5b222e44e40e4306b6fdd
alaurito <andy.laurito@gmail.com>
2016-12-02 17:41:57 +0100

    removed output folder

--- lib/output/copy_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp topology.pdm /home/alaurito/Desktop/repos/powerdevs/examples/Andy
-cp FlowDefinitions.cpp /home/alaurito/Desktop/repos/powerdevs/atomics/PhaseI
-cp flows_definition.scilabParams /home/alaurito/Desktop/repos/powerdevs/examples/Matias/PhaseI/Scilab
-cp links_definition.scilabParams /home/alaurito/Desktop/repos/powerdevs/examples/Matias/PhaseI/Scilab
-cp routers_definition.scilabParams /home/alaurito/Desktop/repos/powerdevs/examples/Matias/PhaseI/Scilab
-cp hosts_definition.scilabParams /home/alaurito/Desktop/repos/powerdevs/examples/Matias/PhaseI/Scilab
