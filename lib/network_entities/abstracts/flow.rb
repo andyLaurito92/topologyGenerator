b99ab3783e861c920b9335cecb4fc8e5eeb47c51
andylaurito <andy.laurito@gmail.com>
2017-10-22 09:57:13 -0300

    adding felix distribution

--- lib/network_entities/abstracts/flow.rb
+++ lib/network_entities/abstracts/flow.rb
@@ -4,6 +4,7 @@ require_relative '../../flows_distributions/exponential_distribution.rb'
 require_relative '../../flows_distributions/normal_distribution.rb'
 require_relative '../../flows_distributions/pareto_distribution.rb'
 require_relative '../../flows_distributions/split_distribution.rb'
+require_relative '../../flows_distributions/felix_distribution.rb'
 
 class Flow
     include SerializeBehavior
@@ -15,8 +16,8 @@ class Flow
         raise "Invalid 'paths' argument received. 'paths' must be an instance of Arry class, however the path received has class #{paths.class}" unless paths.is_a? Array
         raise "Invalid 'distribution_rate' received. Distribution rate cannot be nil" unless distribution_rate
         raise "Invalid 'distribution_size' received. Distribution size cannot be nil" unless distribution_size
-        raise "Invalid 'distribution_rate' #{distribution_rate}. It is expected that the rate is an instance of a distribution" unless [ConstantDistribution, ExponentialDistribution, NormalDistribution, ParetoDistribution, SplitDistribution].include? distribution_rate.class
-        raise "Invalid 'distribution_size' #{distribution_size}. It is expected that the size is an instance of a distribution" unless [ConstantDistribution, ExponentialDistribution, NormalDistribution, ParetoDistribution, SplitDistribution].include? distribution_size.class
+        raise "Invalid 'distribution_rate' #{distribution_rate}. It is expected that the rate is an instance of a distribution" unless [ConstantDistribution, ExponentialDistribution, NormalDistribution, ParetoDistribution, SplitDistribution, FelixDistribution].include? distribution_rate.class
+        raise "Invalid 'distribution_size' #{distribution_size}. It is expected that the size is an instance of a distribution" unless [ConstantDistribution, ExponentialDistribution, NormalDistribution, ParetoDistribution, SplitDistribution, FelixDistribution].include? distribution_size.class
 
         @id = id
         @priority = priority
